0b09726af012895275f5695d8ea52d536916b527
Linda <lindak01@fullsail.edu>
2014-04-30 17:11:11 -0500

    Add CodeIgniter
    Added Codeigniter folders
    Created header/footer
    Created page views

---
+++ Codeigniter/application/views/templates/footer.php
@@ -0,0 +1,5 @@
+
+<strong>&copy; 2014 BDF1404</strong>
+</div>
+</body>
+</html>
---
+++ Codeigniter/application/views/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Be Beautiful Salon</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<link href='http://fonts.googleapis.com/css?family=Tangerine:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="wrapper">
+    <h1>Be Beautiful Salon</h1>
