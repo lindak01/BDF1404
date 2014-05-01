014406dedf75a29421b2c8765a2e6ffa427d43d9
Linda <lindak01@fullsail.edu>
2014-04-30 19:59:33 -0500

    update stylists controller
    Added views to index()

--- Codeigniter/application/controllers/stylists.php
+++ Codeigniter/application/controllers/stylists.php
@@ -11,10 +11,16 @@ class Stylists extends CI_Controller {
     public function index()
     {
         $data['stylist'] = $this->stylists_model->get_stylists();
+        $data['fname'] = 'First name';
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('stylists/index', $data);
+        $this->load->view('templates/footer');
     }
     
     public function view($userId){
         
         $data['stylist'] = $this->stylists_model->get_stylists($userId);
     }
+    
 }
